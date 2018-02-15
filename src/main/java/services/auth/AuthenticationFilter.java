7157e455b24b4ef3c224efe100dd9992b529ea00
patrick kottman <patkottman@gmail.com>
2018-02-15 19:44:12 +0100

    final commit of the day

--- src/main/java/services/auth/AuthenticationFilter.java
+++ src/main/java/services/auth/AuthenticationFilter.java
@@ -1,5 +1,12 @@
 package services.auth;
 
+import services.AuthenticationResource;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerRequestFilter;
+import javax.ws.rs.core.HttpHeaders;
+import java.io.IOException;
+
 public class AuthenticationFilter implements ContainerRequestFilter {
 
     @Override
