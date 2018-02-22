b5e8f052f06f64987e6c8b198032f7708df1f48a
fir3d3vil <timvdlooy@gmail.com>
2018-02-22 13:25:05 +0100

    Js seperated. second test

---
+++ src/main/webapp/js/Login.js
@@ -0,0 +1,33 @@
+$('#login').click(function(event){
+	var data = $('#loginForm').serialize();
+
+	$.post("restservices/authentication/authenticate", data, function(response){
+		window.sessionStorage.setItem("sessionToken", response);
+	}).fail(function(jqXHR, textStatus, errorThrown){
+		console.log(textStatus);
+		console.log(errorThrown);
+		$("#wrongAuthentication").html("Wrong email or password");
+	}).done(function(){
+		var uri = "URI THAT GETS DATA OF MEMBER";
+		 $.ajax(uri, {
+		        type: "POST",
+		        data: $('#loginForm').serialize(),
+		        beforeSend: function(xhr){
+		        	var token = window.sessionStorage.getItem("sessionToken");
+		        	xhr.setRequestHeader('Authorization', 'Bearer ' + token);
+		        },
+		        success: function(response) {
+		            $.each(response, function(key, value){
+		            	window.sessionStorage.setItem("PUTVALUEHERE", value["PUTVALUEHERE"]);
+		            		window.location.replace("https://iacgroep3.herokuapp.com");
+		            });
+		        },
+		        error: function(response) {
+		            $("#response").text("RIP!");
+                window.location.replace("https://iacgroep3.herokuapp.com"); //REMOVE WHEN URI ACTUALLY CONTAINS GOOD URI
+		        }
+		    });
+	});
+
+
+});
