e79e364d1207cece3c34bba0e3197141cecb97d0
fir3d3vil <timvdlooy@gmail.com>
2018-02-23 12:24:06 +0100

    testing getting picture

---
+++ src/main/webapp/js/Categorie.js
@@ -0,0 +1,21 @@
+info();
+function info(){
+	console.log("test");
+	var uri = "restservices/categories/main";
+	 $.ajax(uri, {
+	        type: "GET",
+	        beforeSend: function(xhr){
+	        	var token = window.sessionStorage.getItem("sessionToken");
+	        	xhr.setRequestHeader('Authorization', 'Bearer ' + token);
+	        },
+	        success: function(response) {
+	            $.each(response, function(key, value){
+	            	$("#product-img").html("<img> src=\""+value["picture"]+"\" />");
+								console.log("test");
+	            });
+	        },
+	        error: function(response) {
+	            $("#response").text("RIP!");
+	        }
+	    });
+}
