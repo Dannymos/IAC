dbc39984f8cdc852d68953cb684e8614a3984be3
fir3d3vil <timvdlooy@gmail.com>
2018-03-14 18:16:48 +0100

    God made a little mistake creating human beings

--- src/main/webapp/js/fill_navbar.js
+++ src/main/webapp/js/fill_navbar.js
@@ -2,7 +2,7 @@ $("#navbar").append(
   "<nav>"+
     "<div class=\"nav-wrapper grey lighten-1\">"+
       "<a href=\"index.html\" class=\"brand-logo\">Logo</a>"+
-      "<a href=\"index.html\" data-activates=\"mobile-demo\" class=\"button-collapse\"><i class=\"material-icons\">menu</i></a>"+
+      "<a data-activates=\"mobile-demo\" class=\"button-collapse\"><i class=\"material-icons\">menu</i></a>"+
       "<ul id=\"nav-mobile\" class=\"right hide-on-med-and-down\">"+
         "<li><a href=\"Categorie.html\">Categorie�n</a></li>"+
         "<li><a href=\"Contact.html\">Contact</a></li>"+
