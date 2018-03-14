7628bdd5f2f2120e9c0e3847cc10ff4352f29e29
fir3d3vil <timvdlooy@gmail.com>
2018-03-14 18:53:19 +0100

    And god inserted his define dick into the navbar and created responsivness

--- src/main/webapp/js/fill_navbar.js
+++ src/main/webapp/js/fill_navbar.js
@@ -1,20 +1,23 @@
-$("#navbar").append(
+$("#navbar").html(
   "<nav>"+
     "<div class=\"nav-wrapper grey lighten-1\">"+
       "<a href=\"index.html\" class=\"brand-logo\">Logo</a>"+
       "<a href=\"#\" data-activates=\"mobile-demo\" class=\"button-collapse\"><i class=\"material-icons\">menu</i></a>"+
-      "<ul id=\"nav-mobile\" class=\"right hide-on-med-and-down\">"+
+      "<ul class=\"right hide-on-med-and-down\">"+
         "<li><a href=\"Categorie.html\">Categorie�n</a></li>"+
         "<li><a href=\"Contact.html\">Contact</a></li>"+
         "<li><a href=\"ProductSearch.html\">All products</a></li>"+
-        "<li><a href=\"Afmeleden.html\">Afmelden</a></li>"+
-      "</ul>"+
-      "<ul class=\"side-nav\" id=\"mobile-demo\">"+
-        "<li><a href=\"Categorie.html\">Categorie�n</a></li>"+
-        "<li><a href=\"Contact.html\">Contact</a></li>"+
-        "<li><a href=\"ProductSearch.html\">All products</a></li>"+
-        "<li><a href=\"Afmeleden.html\">Afmelden</a></li>"+
+        "<li><a href=\"Afmelden.html\">Afmelden</a></li>"+
       "</ul>"+
     "</div>"+
   "</nav>"
+);
+
+$("#mobileNavbar").html(
+  "<ul class=\"side-nav\" id=\"mobile-demo\">"+
+    "<li><a href=\"Categorie.html\">Categorie�n</a></li>"+
+    "<li><a href=\"Contact.html\">Contact</a></li>"+
+    "<li><a href=\"ProductSearch.html\">All products</a></li>"+
+    "<li><a href=\"Afmelden.html\">Afmelden</a></li>"+
+  "</ul>"
 );
