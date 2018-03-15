ac03f62fee101f6fd83acffa17b4317c3669d344
fir3d3vil <timvdlooy@gmail.com>
2018-03-15 16:35:52 +0100

    well its starting to look good atleast

--- src/main/webapp/js/ShoppingCard.js
+++ src/main/webapp/js/ShoppingCard.js
@@ -10,12 +10,12 @@ if(sessionStorage.getItem("cart")!=null){
   var el;
   for (el in element) {
     console.log(element[el].price);
-    $("#productsInCard").html(
+    $("#productsInCard").append(
       "<div class=\"row\">"+
         "<div class=\"col s3 m3\" id=\"productImage\"><img class=\"materialboxed\" width=\"150\" src=\"" +element[el].image +"\"></div>"+
-        "<div class=\"col s3 m3\" id=\"productTitle\"><p>"+ element[el].name +"</p></div>"+
-        "<div class=\"col s3 m3\" id=\"productPrice\"><p>"+ element[el].price +"</p></div>"+
-        "<div class=\"col s3 m3\" id=\"productPrice\"><p>"+ element[el].amount +"</p></div>"+
+        "<div class=\"col s2 m2\" id=\"productTitle\"><p>"+ element[el].name +"</p></div>"+
+        "<div class=\"col s2 m2\" id=\"productPrice\"><p>"+ element[el].price +"</p></div>"+
+        "<div class=\"col s2 m2\" id=\"productPrice\"><p>"+ element[el].amount +"</p></div>"+
         "<div class=\"col s3 m3\"><button class=\"waves-effect waves-light btn-large red darken-2\" type=\"button\" id=\"removeProduct\">Remove Product</button></div>"+
       "</div>"
     );
