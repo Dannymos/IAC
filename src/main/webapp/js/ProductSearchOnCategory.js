fc45742cc397ed36dde8f98b44562230d704ea18
fir3d3vil <timvdlooy@gmail.com>
2018-03-16 15:19:18 +0100

    MLG

--- src/main/webapp/js/ProductSearchOnCategory.js
+++ src/main/webapp/js/ProductSearchOnCategory.js
@@ -5,11 +5,11 @@ $('.button-collapse').sideNav({
     draggable: true
 });
 
-SubCategorieInfo();
-function SubCategorieInfo(){
+Producten();
+function Producten(){
   var urlParams = new URLSearchParams(window.location.search);
-  sessionStorage.setItem("Category_id", urlParams.get('id'));
-	var uri = "https://iacgroep3.herokuapp.com/restservices/categories/"+urlParams.get('id');
+  sessionStorage.setItem("subcategory_id", urlParams.get('id'));
+	var uri = "https://iacgroep3.herokuapp.com/restservices/product/categories/"+urlParams.get('id');
 	 $.ajax(uri, {
 	        type: "GET",
 	        beforeSend: function(xhr){
@@ -18,27 +18,28 @@ function SubCategorieInfo(){
 	        },
 	        success: function(response) {
 	            $.each(response, function(key, value){
-	            	$("#cards").append(
+	            	$("#ProductCards").append(
 									"<div class=\"col s12 m6 l3\">"+
 							      "<div class=\"card large\">"+
 							        "<div class=\"card-image\">"+
-							          "<img id=\"product-img\" src=\""+value.picture+"\"/>"+
+							          "<img id=\"product-img\" src=\""+value.image+"\"/>"+
+
 							        "</div>"+
 							        "<div class=\"card-content\">"+
-                        "<span id=\"product-title\" class=\"card-title grey-text text-darken-4\">"+ value.name +"</span>"+
-                        "<p class=\"card-subtitle grey-text text-darken-2\">"+
-												value.description+
+                        "<span id=\"product-title\" class=\"card-title grey-text text-darken-4\">&euro;"+ value.price +"</span>"+
+							          "<p class=\"card-subtitle grey-text text-darken-2\">"+
+												value.name+
 												"</p>"+
 							        "</div>"+
 							       "<div class=\"card-action\">"+
-							          "<a href=\"https://iacgroep3.herokuapp.com/ProductSearchOnCategory.html\?id="+ value.id +"\">Details</a>"+
+							          "<a href=\"https://iacgroep3.herokuapp.com/productinfo.html\?id="+ value.id +"\">Details</a>"+
 							        "</div>"+
 							      "</div>"+
 							   "</div>");
 	            });
 	        },
 	        error: function(response) {
-	            $("#errorHandling").html("The server could not get the sub categories to display. Try logging in again or try again later.");
+	             $("#errorHandling").html("The server could not get any products from our database. Try logging in again or try again later.");;
 	        }
 	    });
 }
