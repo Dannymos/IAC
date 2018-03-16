31e152e676eabaf7ead90a30ffe1be7cbf61bcb0
fir3d3vil <timvdlooy@gmail.com>
2018-03-16 15:38:06 +0100

    lol als dit werkt

--- src/main/webapp/js/fill_navbar.js
+++ src/main/webapp/js/fill_navbar.js
@@ -1,5 +1,5 @@
 $("#navbar").html(
-  "<nav>"+
+  "<nav class=\"nav-extended\">"+
     "<div class=\"nav-wrapper grey lighten-1\">"+
       "<a href=\"index.html\" class=\"brand-logo\">Logo</a>"+
       "<a href=\"#\" data-activates=\"mobile-demo\" class=\"button-collapse\"><i class=\"material-icons\">menu</i></a>"+
@@ -10,9 +10,15 @@ $("#navbar").html(
         "<li><a href=\"ShoppingCard.html\">ShoppingCard</a></li>"+
         "<li><a href=\"Afmelden.html\">Afmelden</a></li>"+
       "</ul>"+
+      "<div class=\"nav-content\">"+
+        "<ul class=\"tabs tabs-transparent\" id=\"navContent\">"+
+        "</ul>"+
+      "</div>"+
     "</div>"+
   "</nav>"
 );
+
+Categorieinfo();
 
 $("#mobileNavbar").html(
   "<ul class=\"side-nav\" id=\"mobile-demo\">"+
@@ -23,3 +29,24 @@ $("#mobileNavbar").html(
     "<li><a href=\"Afmelden.html\">Afmelden</a></li>"+
   "</ul>"
 );
+
+function Categorieinfo(){
+	var uri = "https://iacgroep3.herokuapp.com/restservices/categories/main";
+	 $.ajax(uri, {
+	        type: "GET",
+	        beforeSend: function(xhr){
+	        	var token = window.sessionStorage.getItem("sessionToken");
+	        	xhr.setRequestHeader('Authorization', 'Bearer ' + token);
+	        },
+	        success: function(response) {
+	            $.each(response, function(key, value){
+	            	$("#navContent").append(
+                  "<li class=\"tab\"><a href=\"https://iacgroep3.herokuapp.com/SubCategories.html\?id="+ value.id +"\">"+ value.name +"</a></li>"
+                );
+	            });
+	        },
+	        error: function(response) {
+	            $("#errorHandling").html("The server could not get the main categories to display. Try logging in again or try again later.");
+	        }
+	    });
+}
