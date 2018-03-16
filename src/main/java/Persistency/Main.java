4ef71f0cd4e7af43cd2d9b542b8872d5b2bc0532
patrick kottman <patkottman@gmail.com>
2018-03-16 09:32:03 +0100

    test

--- src/main/java/Persistency/Main.java
+++ src/main/java/Persistency/Main.java
@@ -5,13 +5,16 @@ import Controller.OrderController;
 import Controller.PriceController;
 import Controller.ProductController;
 import Model.Category;
+import services.CategoryService;
 
 public class Main {
     public static void main(String[] args){
 
         OrderController oc = new OrderController();
-
-        System.out.println(oc.addProductToOrder(1,2,4));
+        CategoryController cc = new CategoryController();
+        CategoryService cs = new CategoryService();
+        System.out.println(cs.getMainCategories().getEntity());
+//        System.out.println(oc.addProductToOrder(1,2,4));
 
 
     }
