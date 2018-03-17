a3ae9345c66fd9720017ce3e8c76c4b78997a123
fir3d3vil <timvdlooy@gmail.com>
2018-03-17 15:12:28 +0100

    Ha die resources zijn echt top.......

--- src/main/webapp/js/index.js
+++ src/main/webapp/js/index.js
@@ -4,3 +4,53 @@ $('.button-collapse').sideNav({
     closeOnClick: true,
     draggable: true
 });
+
+OfferProducten();
+function OfferProducten(){
+	var uri = "https://iacgroep3.herokuapp.com/restservices/offer/all";
+	 $.ajax(uri, {
+	        type: "GET",
+	        beforeSend: function(xhr){
+	        	var token = window.sessionStorage.getItem("sessionToken");
+	        	xhr.setRequestHeader('Authorization', 'Bearer ' + token);
+	        },
+	        success: function(response) {
+	            $.each(response, function(key, value){
+                var uri = "https://iacgroep3.herokuapp.com/restservices/product/"+value.product_id;
+              	 $.ajax(uri, {
+              	        type: "GET",
+              	        beforeSend: function(xhr){
+              	        	var token = window.sessionStorage.getItem("sessionToken");
+              	        	xhr.setRequestHeader('Authorization', 'Bearer ' + token);
+              	        },
+              	        success: function(response) {
+                          $("#offerProducts").append(
+          									"<div class=\"col s12 m6 l3\">"+
+          							      "<div class=\"card large\">"+
+          							        "<div class=\"card-image\">"+
+          							          "<img id=\"product-img\" src=\""+response.image+"\"/>"+
+
+          							        "</div>"+
+          							        "<div class=\"card-content\">"+
+                                  "<span id=\"product-title\" class=\"card-title grey-text text-darken-4\">&euro;"+ response.price +"</span>"+
+          							          "<p class=\"card-subtitle grey-text text-darken-2\">"+
+          												response.name+
+          												"</p>"+
+          							        "</div>"+
+          							       "<div class=\"card-action\">"+
+          							          "<a href=\"https://iacgroep3.herokuapp.com/productinfo.html\?id="+ response.id +"\">Details</a>"+
+          							        "</div>"+
+          							      "</div>"+
+          							   "</div>"
+              	        },
+              	        error: function(response) {
+              	             $("#errorHandling").html("The server could not get any products corresponding with the offers from our database.");;
+              	        }
+              	    });
+	            });
+	        },
+	        error: function(response) {
+	             $("#errorHandling").html("The server could not get any products with offers from our database.");;
+	        }
+	    });
+}
