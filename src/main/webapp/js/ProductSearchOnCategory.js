a7bb23cd346b620a26e4405235a8787d8ee48a7c
fir3d3vil <timvdlooy@gmail.com>
2018-03-17 15:44:02 +0100

    some freindly bs

--- src/main/webapp/js/ProductSearchOnCategory.js
+++ src/main/webapp/js/ProductSearchOnCategory.js
@@ -18,24 +18,29 @@ function Producten(){
 	        },
 	        success: function(response) {
 	            $.each(response, function(key, value){
-	            	$("#ProductCards").append(
-									"<div class=\"col s12 m6 l3\">"+
-							      "<div class=\"card large\">"+
-							        "<div class=\"card-image\">"+
-							          "<img id=\"product-img\" src=\""+value.image+"\"/>"+
+                if(response.length == null){
+                  $("#ProductCards").append("<h4>There are no products that fit into this category.</h4>");
+                }
+                else{
+  	            	$("#ProductCards").append(
+  									"<div class=\"col s12 m6 l3\">"+
+  							      "<div class=\"card large\">"+
+  							        "<div class=\"card-image\">"+
+  							          "<img id=\"product-img\" src=\""+value.image+"\"/>"+
 
-							        "</div>"+
-							        "<div class=\"card-content\">"+
-                        "<span id=\"product-title\" class=\"card-title grey-text text-darken-4\">&euro;"+ value.price +"</span>"+
-							          "<p class=\"card-subtitle grey-text text-darken-2\">"+
-												value.name+
-												"</p>"+
-							        "</div>"+
-							       "<div class=\"card-action\">"+
-							          "<a href=\"https://iacgroep3.herokuapp.com/productinfo.html\?id="+ value.id +"\">Details</a>"+
-							        "</div>"+
-							      "</div>"+
-							   "</div>");
+  							        "</div>"+
+  							        "<div class=\"card-content\">"+
+                          "<span id=\"product-title\" class=\"card-title grey-text text-darken-4\">&euro;"+ value.price +"</span>"+
+  							          "<p class=\"card-subtitle grey-text text-darken-2\">"+
+  												value.name+
+  												"</p>"+
+  							        "</div>"+
+  							       "<div class=\"card-action\">"+
+  							          "<a href=\"https://iacgroep3.herokuapp.com/productinfo.html\?id="+ value.id +"\">Details</a>"+
+  							        "</div>"+
+  							      "</div>"+
+  							   "</div>");
+                 }
 	            });
 	        },
 	        error: function(response) {
