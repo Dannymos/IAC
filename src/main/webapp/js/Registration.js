3d46291f8bfd95b093cd119245ddf8f16ce41a87
fir3d3vil <timvdlooy@gmail.com>
2018-03-19 13:07:25 +0100

    mlg shit

--- src/main/webapp/js/Registration.js
+++ src/main/webapp/js/Registration.js
@@ -4,3 +4,26 @@ $('.button-collapse').sideNav({
     closeOnClick: true,
     draggable: true
 });
+
+$("#registerMember").click(function(){
+	var uri = "https://iacgroep3.herokuapp.com/restservices/user/add";
+$.ajax(uri, {
+    type: "POST",
+    data: $("#registerForm").serialize(),
+    beforeSend: function(xhr){
+    	var token = window.sessionStorage.getItem("sessionToken");
+    	xhr.setRequestHeader('Authorization', 'Bearer ' + token);
+    },
+    success: function(response) {
+      if(sessionStorage.getItem("performingOrder")){
+        window.location.replace("OrderConfirment.html");
+      }
+      else{
+        window.location.replace("index.html");
+      }
+    },
+    error: function(response) {
+        $("#errorHandling").html("Adding you to our database went wrong. please try again later.");
+    }
+});
+});
