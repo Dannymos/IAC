e73971749520355a2341211a3240443a22376993
thomasboy020 <thomasboy020@gmail.com>
2018-03-20 10:50:38 +0100

    verwijder producten

--- src/main/webapp/js/EditProduct.js
+++ src/main/webapp/js/EditProduct.js
@@ -36,12 +36,35 @@ function submitGegevens(){
              }
          })
          .done(function() {
-             console.log("success");
+             console.log("Information is changed");
          })
          .fail(function() {
              console.log("failed");
          })
          .always(function() {
-            Materialize.toast('Information is changed', 4000);
+            Materialize.toast('', 4000);
          });
+}
+
+function verwijder(){
+  event.preventDefault();
+ $.ajax({
+           url: '../restservices/product/'+$("#id").val(),
+           type: 'DELETE',
+
+           beforeSend: function(xhr) {
+               var token = window.sessionStorage.getItem("sessionToken");
+               xhr.setRequestHeader('Authorization', 'Bearer ' + token);
+           }
+       })
+       .done(function() {
+           console.log("Product Deleted");
+       })
+       .fail(function() {
+           console.log("failed");
+       })
+       .always(function() {
+          Materialize.toast('', 4000);
+       });
+
 }
