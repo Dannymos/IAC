6e07cc1a9a68fb11af968f3145298f06259f148e
fir3d3vil <timvdlooy@gmail.com>
2018-03-21 15:20:53 +0100

    some dank confirm page

--- src/main/webapp/js/OrderConfirment.js
+++ src/main/webapp/js/OrderConfirment.js
@@ -4,3 +4,28 @@ $('.button-collapse').sideNav({
     closeOnClick: true,
     draggable: true
 });
+
+var element = JSON.parse(sessionStorage.getItem("cart"));
+var totalPrice = 0;
+if(element !== null){
+  if(element.length > 0){
+    var el;
+    for (el in element) {
+      $("#productsInCard").append(
+        "<div class=\"row\">"+
+          "<div class=\"col s3 m3\" id=\"productImage\"><img class=\"materialboxed\" width=\"150\" src=\"" +element[el].image +"\"></div>"+
+          "<div class=\"col s2 m2\" id=\"productTitle\"><p>"+ element[el].name +"</p></div>"+
+          "<div class=\"col s2 m2\" id=\"productPrice\"><p>"+ element[el].price +"</p></div>"+
+          "<div class=\"col s2 m2\" id=\"productPrice\"><p>"+ element[el].amount +"</p></div>"+
+        "</div>"
+      );
+      var totalPrice += element[el].price;
+    }
+    $("#productsInCard").append(
+      "<div class=\"row\">"+
+        "<div class=\"divider col s12 m12\"></div>"+
+        "<div class=\"col s4 m4 push-m8\">&euro;"+ totalPrice +"</div>"+
+      "</div>"
+    );
+  }
+}
