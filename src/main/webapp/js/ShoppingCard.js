6d637bc5e2afe69c332b863a4b38b991cd854951
fir3d3vil <timvdlooy@gmail.com>
2018-03-21 15:37:31 +0100

    MOAAAAR!!!

--- src/main/webapp/js/ShoppingCard.js
+++ src/main/webapp/js/ShoppingCard.js
@@ -12,11 +12,11 @@ if(element !== null){
     for (el in element) {
       $("#productsInCard").append(
         "<div class=\"row\">"+
-          "<div class=\"col s3 m3\" id=\"productImage\"><img class=\"materialboxed\" width=\"150\" src=\"" +element[el].image +"\"></div>"+
-          "<div class=\"col s2 m2\" id=\"productTitle\"><p>"+ element[el].name +"</p></div>"+
-          "<div class=\"col s2 m2\" id=\"productPrice\"><p>Price: &euro;"+ element[el].price +"</p></div>"+
-          "<div class=\"col s2 m2\" id=\"productPrice\"><p>Amount: "+ element[el].amount +"</p></div>"+
-          "<div class=\"col s3 m3\"><button class=\"waves-effect waves-light btn-large red darken-2\" type=\"button\" id=\"removeProduct\" onclick=\"removeProduct("+ element[el].id +")\">Remove Product</button></div>"+
+          "<div class=\"col s12 m3\" id=\"productImage\"><img class=\"materialboxed\" width=\"150\" src=\"" +element[el].image +"\"></div>"+
+          "<div class=\"col s12 m2\" id=\"productTitle\"><p>"+ element[el].name +"</p></div>"+
+          "<div class=\"col s12 m2\" id=\"productPrice\"><p>Price: &euro;"+ element[el].price +"</p></div>"+
+          "<div class=\"col s12 m2\" id=\"productPrice\"><p>Amount: "+ element[el].amount +"</p></div>"+
+          "<div class=\"col s12 m3\"><button class=\"waves-effect waves-light btn-large red darken-2\" type=\"button\" id=\"removeProduct\" onclick=\"removeProduct("+ element[el].id +")\">Remove Product</button></div>"+
         "</div>"
       );
     }
