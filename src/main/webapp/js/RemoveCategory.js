77af8c71ef94ca1ef8af186a4a3e54ee33befd9f
patrick kottman <patkottman@gmail.com>
2018-03-22 21:23:47 +0100

    sum testing

--- src/main/webapp/js/RemoveCategory.js
+++ src/main/webapp/js/RemoveCategory.js
@@ -14,7 +14,9 @@ $("#getcategoriesforproduct").click( (e)=> {
         }
     })
         .done(function (data) {
+            console.log(data);
             $(data).each(() => {
+                console.log(this);
                 $('#categories').empty();
                 $('#categories').append(
                     "<li class=\"collection-item\"><div>" + this.name + "<a class=\" removebutton data-category=\"" + this.id + "\" href=\"#!\">delete me</a></div></li>"
