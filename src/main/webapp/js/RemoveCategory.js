a6ad9109fe776ae116d354a719e4e3e42910a898
patrick kottman <patkottman@gmail.com>
2018-03-22 21:07:08 +0100

    maybe this works

--- src/main/webapp/js/RemoveCategory.js
+++ src/main/webapp/js/RemoveCategory.js
@@ -14,7 +14,7 @@ $("#getcategoriesforproduct").click( (e)=> {
         }
     })
         .done(function (data) {
-            $(data).each(data, (index) => {
+            $(data).each( () => {
                 $('#categories').empty();
                 $('#categories').append(
                     "<li class=\"collection-item\"><div>" + this.name + "<a class=\" removebutton data-category=\"" + this.id + "\" href=\"#!\">delete me</a></div></li>"
