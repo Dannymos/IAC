c8d588d2cfbee8ffb8b38727e4a2df9383554602
patrick kottman <patkottman@gmail.com>
2018-03-22 19:54:51 +0100

    removed a line

--- src/main/java/services/ProductResource.java
+++ src/main/java/services/ProductResource.java
@@ -50,7 +50,7 @@ public class ProductResource {
 
     @POST
     @Path("/add")
-    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+
     public Response addProduct(@FormParam("name")String name,@FormParam("price")double price,@FormParam("explanation")String explanation,@FormParam("image")String image) {
         Product product = aProduct()
                 .setName(name)
