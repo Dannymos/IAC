25e5a16a5b4bdb1f516f7c5032a7b39f3785606b
fir3d3vil <timvdlooy@gmail.com>
2018-03-22 23:24:05 +0100

    oops

--- src/main/webapp/js/fill_navbar.js
+++ src/main/webapp/js/fill_navbar.js
@@ -13,7 +13,7 @@ $("#navbar").html(
         "<li><a href=\"Contact.html\">Contact</a></li>"+
         "<li><a href=\"ProductSearch.html\">All products</a></li>"+
         "<li><a href=\"ShoppingCard.html\">ShoppingCard</a></li>"+
-        "<li><a id=\"logOut\" href=\"index.html\">Log out</a></li>"+
+        "<li><a id=\"logOut\" href=\"index.html\">"+LogedIn+"</a></li>"+
       "</ul>"+
       "<ul class=\"side-nav\" id=\"mobile-demo\">"+
         "<li><a href=\"Categorie.html\">Categories</a></li>"+
