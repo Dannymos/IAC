c2b6f604be1e0f5be3b9f5011bfea4d4b2b3135f
thomasboy020 <thomasboy020@gmail.com>
2018-03-22 11:06:00 +0100

    category toevoegen

--- src/main/webapp/js/AddCategory.js
+++ src/main/webapp/js/AddCategory.js
@@ -4,3 +4,27 @@ $('.button-collapse').sideNav({
     closeOnClick: true,
     draggable: true
 });
+
+function loadGegevens(){
+  $.ajax({
+            url: "../restservices/categories/subcategories",
+            type: 'GET',
+            beforeSend: function(xhr) {
+                var token = window.sessionStorage.getItem("sessionToken");
+                xhr.setRequestHeader('Authorization', 'Bearer ' + token);
+            }
+          });
+            var $select = $("#select");
+            $.each(result, function() {
+                $select.append($("<option />").val(this.id).text(this.name));
+
+            });
+
+
+}
+
+
+
+$(document).ready(function() {
+    $('select').material_select();
+  });
