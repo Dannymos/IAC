562953aac41d0118e9f15383ac777467b920560f
Dannymos <dannymoswow@gmail.com>
2018-03-22 21:55:04 +0100

    Fix

--- src/main/webapp/js/Registration.js
+++ src/main/webapp/js/Registration.js
@@ -6,7 +6,7 @@ $('.button-collapse').sideNav({
 });
 
 $("#registerMember").click(function(){
-	var uri = "https://iacgroep3.herokuapp.com/restservices/user/add";
+	var uri = "https://iacgroep3.herokuapp.com/restservices/user/register";
 $.ajax(uri, {
     type: "POST",
     data: $("#registerForm").serialize(),
@@ -18,6 +18,7 @@ $.ajax(uri, {
       if(sessionStorage.getItem("performingOrder")){
         sessionStorage.removeItem("performingOrder");
         //window.location.replace("https://iacgroep3.herokuapp.com/OrderConfirment.html");                  REMOVE "//" WHEN NO ERRORS ARE GENERATED
+          console.log("Succesfully registered!");
       }
       else{
         //window.location.replace("https://iacgroep3.herokuapp.com/index.html");                            REMOVE "//" WHEN NO ERRORS ARE GENERATED
