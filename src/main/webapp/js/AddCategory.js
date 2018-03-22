c2745def42f750eb86ade63ea6d6cfa6261f3836
thomasboy020 <thomasboy020@gmail.com>
2018-03-22 11:35:21 +0100

    test

--- src/main/webapp/js/AddCategory.js
+++ src/main/webapp/js/AddCategory.js
@@ -12,17 +12,44 @@ $( document ).ready(function loadCategory(){
             beforeSend: function(xhr) {
                 var token = window.sessionStorage.getItem("sessionToken");
                 xhr.setRequestHeader('Authorization', 'Bearer ' + token);
-            }, success: function(result) {
+            }
+          })
+
+
+
+            .success(function(result) {
               var $select = $("#select");
               $.each(result, function() {
                   $select.append($("<option />").val(this.id).text(this.name));
 
               });
+
+});
+});
+
+function loadGegevens(){
+  $.ajax({
+            url: "../restservices/product/" + $("#id").val(),
+            type: 'GET',
+            beforeSend: function(xhr) {
+                var token = window.sessionStorage.getItem("sessionToken");
+                xhr.setRequestHeader('Authorization', 'Bearer ' + token);
             }
-});
+            })
+            .done(function(data) {
+                        $(data).each(function(index) {
+                            $("#name").val(this.name);
+                            $("#price").val(this.price);
+                            $("#explanation").val(this.explanation);
+                            $("#image").val(this.image);
 
 
+                            Materialize.updateTextFields();
+
+                        });
+
 });
+}
 
 
 
