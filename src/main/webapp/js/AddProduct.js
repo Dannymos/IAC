2d52e48a41216396e4423b1b6046e356b6d2d507
thomasboy020 <thomasboy020@gmail.com>
2018-03-22 13:43:41 +0100

    add product

--- src/main/webapp/js/AddProduct.js
+++ src/main/webapp/js/AddProduct.js
@@ -1,6 +1,23 @@
-$('.button-collapse').sideNav({
-    menuWidth: 300,
-    edge: 'left',
-    closeOnClick: true,
-    draggable: true
-});
+function submitProduct(){
+    event.preventDefault();
+  var data = $("#submitform").serialize();
+   $.ajax({
+             url: '../restservices/product/'+$("#id").val(),
+             type: 'PUT',
+             data: data,
+
+             beforeSend: function(xhr) {
+                 var token = window.sessionStorage.getItem("sessionToken");
+                 xhr.setRequestHeader('Authorization', 'Bearer ' + token);
+             }
+         })
+         .done(function() {
+             console.log("Information is changed");
+         })
+         .fail(function() {
+             console.log("failed");
+         })
+         .always(function() {
+            Materialize.toast('', 4000);
+         });
+}
