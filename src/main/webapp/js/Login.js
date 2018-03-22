441c37fecdc1c89e660d4196ceed986f09075c5e
fir3d3vil <timvdlooy@gmail.com>
2018-03-22 10:43:41 +0100

    Login

--- src/main/webapp/js/Login.js
+++ src/main/webapp/js/Login.js
@@ -4,23 +4,6 @@ $('.button-collapse').sideNav({
     closeOnClick: true,
     draggable: true
 });
-/*
-$('#login').click(function(event){
-	var data = $('#loginForm').serialize();
-	$.post("https://iacgroep3.herokuapp.com/restservices/authentication/authenticate", data, function(response){
-		console.log(response);
-		var tokenAndId = JSON.parse(response);
-		console.log(tokenAndId);
-		console.log(tokenAndId.id);
-		console.log(tokenAndId.token);
-		window.sessionStorage.setItem("sessionToken", tokenAndId.token);
-		window.sessionStorage.setItem("id", tokenAndId.id);
-	}).fail(function(jqXHR, textStatus, errorThrown){
-		console.log(textStatus);
-		console.log(errorThrown);
-		console.log("Try logging in again");
-	});
-});*/
 
 $('#login').click(function(event){
 	var uri = "https://iacgroep3.herokuapp.com/restservices/authentication/authenticate";
@@ -28,18 +11,17 @@ $('#login').click(function(event){
 	        type: "POST",
           data: $("#loginForm").serialize(),
 	        success: function(response) {
-            console.log(response);
         		var tokenAndId = JSON.parse(response);
-        		console.log(tokenAndId);
-        		console.log(tokenAndId.id);
-        		console.log(tokenAndId.token);
         		window.sessionStorage.setItem("sessionToken", tokenAndId.token);
         		window.sessionStorage.setItem("id", tokenAndId.id);
+            console.log(tokenAndId.role);
+            window.sessionStorage.setItem("role", tokenAndId.role);
           },
 	        error: function(response) {
             console.log(textStatus);
             console.log(errorThrown);
             console.log("Try logging in again");
+            $("#wrongLogin").html("Wrong password email combination");
 	        }
 	    });
     });
