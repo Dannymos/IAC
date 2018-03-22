e1cfb26ddd3365a89bae51cec5c5de7b6aa4c3d3
fir3d3vil <timvdlooy@gmail.com>
2018-03-22 11:24:42 +0100

    rest in a rest makes double rest

--- src/main/webapp/js/OrderConfirment.js
+++ src/main/webapp/js/OrderConfirment.js
@@ -31,5 +31,30 @@ if(element !== null){
 }
 
 $('#orderConfirmed').click(function(event){
-  //ADD THEM ORDERS M8
+  var uri = "https://iacgroep3.herokuapp.com/restservices/user/"+sessionStorage.getItem("id");
+   $.ajax(uri, {
+          type: "GET",
+          beforeSend: function(xhr){
+            var token = window.sessionStorage.getItem("sessionToken");
+            xhr.setRequestHeader('Authorization', 'Bearer ' + token);
+          },
+          success: function(response) {
+            console.log(response);
+            var uri = "https://iacgroep3.herokuapp.com/restservices/order/complete?CustomerID="+sessionStorage.getItem("id")+"&deliveryAddress="+response.deliveryAddress;
+             $.ajax(uri, {
+                    type: "POST",
+                    beforeSend: function(xhr){
+                      var token = window.sessionStorage.getItem("sessionToken");
+                      xhr.setRequestHeader('Authorization', 'Bearer ' + token);
+                    },
+                    success: function(response) {
+                      console.log("Order created");
+                    },
+                    error: function(response) {
+                      $("#errorHandling").html("The server could not create a order. Please login again or try again at a later time.");
+                });
+          },
+          error: function(response) {
+
+      });
 });
